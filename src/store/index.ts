d05469e0c1e76d0e8af7190142c444a5dcf1a3c2
Sergei Panfilov <sergei.panfilov@powerhouse.nl>
2019-11-12 16:40:47 +0100

    vuex steps

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,33 @@
+import { Item } from '@/models/Item'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const items: ReadonlyArray<Item> = [
+  { id: 1, title: 'Super Item 1', price: 20, desc: 'qqqqqq' },
+  { id: 2, title: 'Awesome Item 2', price: 30, desc: 'wwwwww' },
+  { id: 3, title: 'Just another handsome Item 3', price: 40, desc: 'eeeee' }
+]
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    items
+  },
+  getters: {
+    // getItems: state => {
+    //   return state.items.filter(todo => todo.done)
+    // }
+  },
+  mutations: {
+    addItem(item: Item): void {
+      state = {...state.items, item }
+    }
+  },
+  actions: {
+    addItem(item: Item): void {
+      commit('addItem', item)
+    }
+  },
   modules: {}
 })
