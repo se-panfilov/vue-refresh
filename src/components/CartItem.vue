ad8bf2ef706a3e7481b8bfc74590894bdb701092
Sergei Panfilov <se-panfilov@ya.ru>
2019-12-09 22:30:44 +0100

    vuex improvements

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,34 +1,39 @@
 <template>
-    <li class="collection-item avatar">
-        <div class="item-img">
-<!--            {/*<img src={props.img} alt={props.img} class=""/>*/}-->
-        </div>
+  <li class="collection-item avatar">
+    <div class="item-img">
+      <!--            {/*<img src={item.img} alt={item.img} class=""/>*/}-->
+    </div>
 
-        <div class="item-desc">
-            <span class="title">{props.title}</span>
-            <p>{props.desc}</p>
-            <p><b>Price: {props.price}$</b></p>
-            <p>
-                <b>Quantity: {props.quantity}</b>
-            </p>
-            <div class="add-remove">
-                <router-link to="/cart"><i class="material-icons">arrow_drop_up</i></router-link>
-                <router-link to="/cart"><i class="material-icons">arrow_drop_down</i></router-link>
-            </div>
-<!--            <button type="button"-->
-<!--                    class="waves-effect waves-light btn pink remove"-->
-<!--                    onClick={props.onRemoveClick}>-->
-<!--                Remove-->
-<!--            </button>-->
-        </div>
+    <div class="item-desc">
+      <span class="title">{{item.title}}</span>
+      <p>{item.desc}</p>
+      <p><b>Price: {{item.price}}$</b></p>
+      <p>
+        <b>Quantity: {{item.quantity}}</b>
+      </p>
+      <div class="add-remove">
+        <router-link to="/cart"><i class="material-icons">arrow_drop_up</i></router-link>
+        <router-link to="/cart"><i class="material-icons">arrow_drop_down</i></router-link>
+      </div>
+      <!--            <button type="button"-->
+      <!--                    class="waves-effect waves-light btn pink remove"-->
+      <!--                    onClick={item.onRemoveClick}>-->
+      <!--                Remove-->
+      <!--            </button>-->
+    </div>
 
-    </li>
+  </li>
 </template>
 
 <script lang="ts">
+import {Prop} from 'vue/types/options';
+import {BasketItem} from '../models/Item';
 
-  export default {
-    name: 'cart-item',
-    components: {}
-  }
+export default {
+  name: 'cart-item',
+  props: {
+    item: Object as Prop<BasketItem>
+  },
+  components: {}
+};
 </script>
