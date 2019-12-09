b09d489d22c1ad2d68756f90db4c8ccdb093f38a
Sergei Panfilov <se-panfilov@ya.ru>
2019-12-09 22:35:44 +0100

    vuex improvements

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,34 +1,56 @@
-import { Item } from '@/models/Item'
-import Vue from 'vue'
-import Vuex from 'vuex'
+import {BasketItem, Item} from '@/models/Item';
+import Vue from 'vue';
+import Vuex, {StoreOptions} from 'vuex';
 
-Vue.use(Vuex)
+Vue.config.devtools = process.env.NODE_ENV === 'development';
 
-const items: ReadonlyArray<Item> = [
-  { id: 1, title: 'Super Item 1', price: 20, desc: 'qqqqqq' },
-  { id: 2, title: 'Awesome Item 2', price: 30, desc: 'wwwwww' },
-  { id: 3, title: 'Just another handsome Item 3', price: 40, desc: 'eeeee' }
-]
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const availableItems: ReadonlyArray<Item> = [
+  {id: 1, title: 'Super Item 1', price: 20, desc: 'qqqqqq'},
+  {id: 2, title: 'Awesome Item 2', price: 30, desc: 'wwwwww'},
+  {id: 3, title: 'Just another handsome Item 3', price: 40, desc: 'eeeee'}
+];
+
+const storeOptions: StoreOptions<State> = {
   state: {
-    items
+    availableItems,
+    basketItems: []
   },
   getters: {
-    // getItems: state => {
-    //   return state.items.filter(v => v.done)
-    // }
+    basketItemsCount: state => state.basketItems.reduce((acc, cur) => acc += cur.quantity, 0)
   },
   mutations: {
-    addItem(state, item: Item): void {
-      const items = {...state.items, item};
-      state = {...state, items }
+    addItem(state: State, item: Item): void {
+      const existedItemIndex = state.basketItems.findIndex(i => i.id === item.id);
+      let updatedItem: BasketItem | undefined;
+      let basketItems = [];
+
+      if (existedItemIndex === -1) {
+        updatedItem = {...item, quantity: 1};
+        basketItems = [...state.basketItems, updatedItem];
+      } else {
+        updatedItem = {
+          ...state.basketItems[existedItemIndex],
+          quantity: state.basketItems[existedItemIndex].quantity + 1
+        };
+        basketItems = [...state.basketItems.slice(0, existedItemIndex), updatedItem, ...state.basketItems.slice(existedItemIndex+1)];
+      }
+
+      Vue.set(state, 'basketItems', basketItems);
     }
   },
   actions: {
     addItem(context, item: Item): void {
-      context.commit('addItem', item)
+      context.commit('addItem', item);
     }
   },
   modules: {}
-})
+};
+
+export default new Vuex.Store(storeOptions);
+
+export interface State {
+  availableItems: ReadonlyArray<Item>,
+  basketItems: ReadonlyArray<BasketItem>
+}
